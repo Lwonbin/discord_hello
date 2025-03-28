94c28bf0ac6b4fd81b1c46b53cb58c4d8d8322a3
sungjoon <dkdldrha@naver.com>
2025-03-28 16:15:35 +0900

    demo

--- src/main/java/discord/demo/HelloController.java
+++ src/main/java/discord/demo/HelloController.java
@@ -1,14 +1,26 @@
 package discord.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.servlet.function.ServerResponse.status;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> getConsentPolicy() {
-        return ResponseEntity.ok("Hello World");
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    @PostMapping("/hello")
+    public ResponseEntity<Data> getConsentPolicy(@RequestBody Data data) {
+
+        log.info(data.getData());
+        return ResponseEntity.status(HttpStatus.OK).body(data);
     }
 }
